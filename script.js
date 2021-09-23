b726cd91a67d954465b0e1fa855f8490cb37f2b1
Denis Gitko <prostodenis1397@gmail.com>
2021-09-23 11:27:28 +0800

    added functionality to complete button, added Date to each task

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const btnDelete = document.querySelector("#btn--delete");
 
 addBtn.addEventListener("click", addTask);
 list.addEventListener("click", deleteTask);
-
+list.addEventListener("click", completeTask);
 
 function addTask() {
     // If the input isn't empty, do the following
@@ -19,6 +19,60 @@ function addTask() {
         // Create a new li for the task
             let newTask = document.createElement("li");
             newTask.classList.add("app__item");
+        // Create a container for the text and Date, to put space between them and the buttons
+            let appInner = document.createElement("div");
+            appInner.classList.add("app__item-inner");
+         // Create span with the user input text
+            let newText = document.createElement("span");
+            newText.classList.add("app__item-text");
+            newText.textContent = inputText.value;
+
+    /**********************  Getting Date start **************************/
+        //  Create an h3 for Date
+            let time = document.createElement("h3");
+            let date = new Date();
+            let days = date.getDate();
+            days = days < 10 ? "0" + days : days;
+            let months = date.getMonth();
+            // Converting month into 12 month form and adding a 0 at the beginning if it's a month between 1 and 9
+            months = months < 10 ? "0" + (months + 1) : months + 1;
+
+            let years = date.getFullYear();
+            let hours = date.getHours();
+            if(hours > 0 && hours < 12) {
+                hours = hours;
+            } if(hours >= 13 && hours <= 23) {
+                hours = hours - 12;
+            } if(hours === 12) {
+                hours = 12;
+            } if(hours === 0) {
+                hours = 12;
+            }
+            let minutes = date.getMinutes();
+
+            // Get AM or PM depending on date.getHours property
+            let amPm = "";
+            if(date.getHours() > 0 && hours < 12) {
+                amPm = "AM";
+            } if(date.getHours() >= 13 && hours <= 23) {
+                amPm = "PM";
+            } if(date.getHours() === 12) {
+                amPm = "PM";
+            } if(date.getHours() === 0) {
+                amPm = "AM";
+            }
+
+
+            time.innerHTML = `${months}.${days}.${years}, ${hours}:${minutes} ${amPm}`;
+            time.classList.add("time");
+            console.log(time);
+    
+    /**********************  Getting Date finish **************************/
+
+
+        // Append the user input and Date to a container div
+            appInner.append(newText, time);
+
         // Create buttons and container for them
             let btnContainer = document.createElement("div");
             btnContainer.classList.add("#app__item-btns");
@@ -32,14 +86,14 @@ function addTask() {
             newDeleteBtn.innerHTML = "<i class='fas fa-times-circle'></i>"
 
             btnContainer.append(newCompleteBtn, newDeleteBtn);
-        // Create span with the user input text
-            let newText = document.createElement("span");
-            newText.classList.add("app__item-text");
-            newText.textContent = inputText.value;
-        // Append the info and buttons to the li
-            newTask.append(newText, btnContainer);
+       
+        // Append the container to the li
+  
+            newTask.append(appInner, btnContainer);
         // Append the li to the ul
             list.append(newTask);
+
+
 
     // If the input is empty, tell the user to enter the info first    
     } else {
@@ -51,6 +105,8 @@ function addTask() {
 }
 
 
+
+
 function deleteTask(e) {
     // Check if the clicked spot was an icon with class fa-times-circle and delete the task if it is 
     if(e.target.classList.contains("fa-times-circle")) {
@@ -60,6 +116,18 @@ function deleteTask(e) {
     if(e.target.classList.contains("btn--delete")) {
         list.removeChild(e.target.parentElement.parentElement);
     }
-
-    console.log(e.target);
 }
+
+
+function completeTask(e) {
+    if(e.target.classList.contains("fa-check-circle")) {
+      e.target.parentElement.parentElement.parentElement.classList.toggle("app__item--completed");
+
+    };
+}
+
+function getDate() {
+    console.log("working");
+}
+
+console.log(new Date().getDate());
